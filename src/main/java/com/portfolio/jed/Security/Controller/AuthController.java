60a8500952036ec4722edf8bc87c76d113d3dd30
Jontuc <jediaz.tp@gmail.com>
2022-08-01 12:50:22 -0300

    Subida Proyecto BackEnd

--- src/main/java/com/portfolio/jed/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/jed/Security/Controller/AuthController.java
@@ -4,7 +4,11 @@
  */
 package com.portfolio.jed.Security.Controller;
 
+import com.portfolio.jed.Security.Dto.JwtDTO;
+import com.portfolio.jed.Security.Dto.LoginUsuario;
+import com.portfolio.jed.Security.Dto.NuevoUsuario;
 import com.portfolio.jed.Security.Entity.Rol;
+import com.portfolio.jed.Security.Entity.Usuario;
 import com.portfolio.jed.Security.Enums.RolNombre;
 import com.portfolio.jed.Security.Service.RolService;
 import com.portfolio.jed.Security.Service.UsuarioService;
@@ -16,6 +20,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -56,7 +64,7 @@ public class AuthController {
             return new ResponseEntity(new Mensaje("Ese email ya existe"), HttpStatus.BAD_REQUEST);
         }
 
-        Usuario usuario = Usuario(nuevoUsuario.getNombre(), nuevoUsuario.getNombreUsuario(),
+        Usuario usuario = new Usuario(nuevoUsuario.getNombre(), nuevoUsuario.getNombreUsuario(),
                 nuevoUsuario.getEmail(), passwordEncoder.encode(nuevoUsuario.getPassword()));
         Set<Rol> roles = new HashSet<>();
         roles.add(rolService.getByRolNombre(RolNombre.ROLE_USER).get());
@@ -69,7 +77,22 @@ public class AuthController {
         return new ResponseEntity(new Mensaje("Usuario guardado"),HttpStatus.CREATED);
     }
     
-    public ResponseEntity<JwtDTO> login(@Validate @RequestBody LoginUsuario loginUsuario, BindingResult bindingResult){
+    @PostMapping("/login")
+    public ResponseEntity<JwtDTO> login(@Valid @RequestBody LoginUsuario loginUsuario, BindingResult bindingResult){
+        if(bindingResult.hasErrors())
+            return new ResponseEntity(new Mensaje("Campos mal puestos"),HttpStatus.BAD_REQUEST);
         
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                loginUsuario.getNombreUsuario(), loginUsuario.getPassword()));
+        
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        
+        String jwt = jwtProvider.generateToken(authentication);
+        
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        
+        JwtDTO jwtDto = new JwtDTO(jwt, userDetails.getUsername(), userDetails.getAuthorities());
+        
+        return new ResponseEntity(jwtDto, HttpStatus.OK);
     }
 }
